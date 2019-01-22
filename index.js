03c39648df335731b468536f8ff77f9c1157bf34
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-01-22 13:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++){
-    return 
+    return ``
   }
 }
